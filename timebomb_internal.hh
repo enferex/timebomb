26544655aa71b0f20bd8a73e5a47f3bc7d2bd26c
Matt Davis <mattdavis9@gmail.com>
2024-03-16 14:46:38 +0000

    Rename the header guard

--- timebomb_internal.hh
+++ timebomb_internal.hh
@@ -1,10 +1,10 @@
 // This is free and unencumbered software released into the public domain.
-// 
+//
 // Anyone is free to copy, modify, publish, use, compile, sell, or
 // distribute this software, either in source code form or as a compiled
 // binary, for any purpose, commercial or non-commercial, and by any
 // means.
-// 
+//
 // In jurisdictions that recognize copyright laws, the author or authors
 // of this software dedicate any and all copyright interest in the
 // software to the public domain. We make this dedication for the benefit
@@ -12,7 +12,7 @@
 // successors. We intend this dedication to be an overt act of
 // relinquishment in perpetuity of all present and future rights to this
 // software under copyright law.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -20,11 +20,11 @@
 // OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
-// 
+//
 // For more information, please refer to <https://unlicense.org>
 
-#ifndef __TIMEBOMB_INTERNAL__HH
-#define __TIMEBOMB_INTERNAL__HH
+#ifndef __TIMEBOMB_INTERNAL_HH
+#define __TIMEBOMB_INTERNAL_HH
 
 // This file contains the primary functionality of this timebomb library and
 // is not intended to be used directly.
@@ -102,4 +102,4 @@ consteval bool __tbIsDate2(const char *aStr, const timebomb::Date d) {
   return a.month == d.month && a.day == d.day && a.year == d.year;
 }
 
-#endif  //  __TIMEBOMB_INTERNAL__HH
+#endif  //  __TIMEBOMB_INTERNAL_HH
